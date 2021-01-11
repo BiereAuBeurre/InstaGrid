a1737753f8c5f4b2db17d455c525b9468630506f
Manon Russo <russo.manon@gmail.com>
2021-01-11 11:13:54 +0100

    correction bouton , création d'outlet collection pour les photo button et les layout buttons

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -8,56 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    @IBOutlet weak var holderSwipeView: UIStackView!
     
     @IBOutlet weak var arrowSwipeView: UIImageView!
-    
-    
+
     @IBOutlet weak var textSwipeView: UIButton!
-    
-    @IBOutlet weak var holderMainView: UIView!
-    
-    
-    @IBOutlet weak var photoButton1: UIButton!
-    
-    @IBOutlet weak var photoButton2: UIButton!
-    
-    @IBOutlet weak var photoButton3: UIButton!
-    
-    @IBOutlet weak var photoButton4: UIButton!
-    
-    @IBOutlet weak var holderLayoutsView: UIStackView!
-    
-    @IBOutlet weak var layoutButton1: UIButton!
-    
-    @IBOutlet weak var layoutButton2: UIButton!
-    
-    @IBOutlet weak var layoutButton3: UIButton!
+ 
+
+    @IBOutlet var photoButtons: [UIButton]!
+    @IBOutlet var layoutButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    
+    @IBAction func didTapLayoutButton(_ sender: UIButton) {
+        for button in layoutButtons {
+            button.isSelected = false
+        }
+        sender.isSelected = true
+        
+        if sender == layoutButtons[0] {
+            photoButtons[0].isHidden = true
+            photoButtons[3].isHidden = false
 
-    @IBAction func buttonLayout1Pressed(_ sender: UIButton) {
-        sender.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
-        photoButton1.isHidden = true
-        photoButton3.isHidden = false
+        } else if sender == layoutButtons[1] {
+            photoButtons[3].isHidden = true
+            photoButtons[0].isHidden = false
+        } else {
+            photoButtons[0].isHidden = false
+            photoButtons[3].isHidden = false
+        }
     }
     
-    @IBAction func buttonLayout2Pressed(_ sender: UIButton) {
-        sender.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
-        photoButton1.isHidden = false
-        photoButton3.isHidden = true
-    }
+/*
     
     @IBAction func buttonLayout3Pressed(_ sender: UIButton) {
         sender.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
         photoButton1.isHidden = false
         photoButton3.isHidden = false
     }
-    
+    */
 }
 
