de06e43523728a879cd295c5636cf171bef5f1c9
Manon Russo <russo.manon@gmail.com>
2021-01-11 15:10:18 +0100

    ajout de la fonctionnalité librairie sur la mainView ok, reste à recadrer les images bien

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -7,15 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var arrowSwipeView: UIImageView!
-
     @IBOutlet weak var textSwipeView: UIButton!
  
+    var pictureButton: UIButton!
 
     @IBOutlet var photoButtons: [UIButton]!
     @IBOutlet var layoutButtons: [UIButton]!
+    
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +34,6 @@ class ViewController: UIViewController {
         if sender == layoutButtons[0] {
             photoButtons[0].isHidden = true
             photoButtons[3].isHidden = false
-
         } else if sender == layoutButtons[1] {
             photoButtons[3].isHidden = true
             photoButtons[0].isHidden = false
@@ -42,13 +43,28 @@ class ViewController: UIViewController {
         }
     }
     
-/*
-    
-    @IBAction func buttonLayout3Pressed(_ sender: UIButton) {
-        sender.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
-        photoButton1.isHidden = false
-        photoButton3.isHidden = false
+    @IBAction func didTapMainViewbutton(_ sender: UIButton) {
+        pictureButton = sender
+        let imagePicker = UIImagePickerController()
+        /// Quand touché, ouvre le menu pour sélectionner une image -> Ok
+        /// Choisir une image -> Ok
+        /// L'adapter au bouton (fill) -> /!\ à faire /!\
+        /// Remplacer "+" par 'image choisie -> ok
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
     }
-    */
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true) { [weak self] in
+            if let image = info[.originalImage] as? UIImage {
+                self?.pictureButton?.setImage(image, for: .normal)
+                self?.pictureButton?.imageView?.contentMode = .scaleAspectFit
+            }
+        }
+    }
+    
 }
+
 
